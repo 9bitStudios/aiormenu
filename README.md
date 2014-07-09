5ad92dbaa52c488fad712145cb5f7257bb7a0373
Ian C <9bitStudios@gmail.com>
2014-07-09 05:56:33 -0700

    Updates to README.md...

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-aiormenu
+All-in-One Responsive Menu
 ========
 
-All-in-One Responsive Menu
+All-in-One Responsive Menu (aiormenu) is a jQuery plugin that will take a list menu and create a responsive menu out of that list for smaller viewing widths (e.g. mobile and tablets). There are a number of different types of responsive menus you can choose from.
--- js/jquery.aiormenu.js
+++ js/jquery.aiormenu.js
@@ -3,7 +3,7 @@
 * Version: 1.0.0
 * Description: AIOR (All in One Responsive) Menu - jQuery Plugin to create responsive menus from a given menu
 * Author: 9bit Studios
-* Copyright 2012, 9bit Studios
+* Copyright 2014, 9bit Studios
 * http://www.9bitstudios.com
 * Free to use and abuse under the MIT license.
 * http://www.opensource.org/licenses/mit-license.php
@@ -14,184 +14,183 @@
     $.fn.aiormenu = function (options) {
 
         var defaults = $.extend({
-			targetContainer: null,			
-        	type: 'select',
-			menuID: 'responsiveMenu',
+            targetContainer: null,            
+            type: 'select',
+            menuID: 'responsiveMenu',
         }, options);
         
-		/******************************
-		Private Variables
-		*******************************/         
+        /******************************
+        Private Variables
+        *******************************/         
         
         var object = $(this);
-		var settings = $.extend(defaults, options);           
+        var settings = $.extend(defaults, options);           
         
         
-		/******************************
-		Public Methods
-		*******************************/        
+        /******************************
+        Public Methods
+        *******************************/        
         
         var methods = {
-        		
-			init: function() {
-				
-        		return this.each(function () {
-        			methods.appendHTML();
-        			methods.setEventHandlers();      			
-				});
-			},
-			
-			/******************************
-			Append HTML
-			*******************************/			
-			
-			appendHTML: function() {
-			
-				switch(settings.type)
-				{
-					case 'select':
-						methods.createSelectBox();
-						break;
-					  
-					case 'list':
-						methods.createList();
-						break;
-					
-					case 'panel':
-						methods.createPanel();
-						break;
-					
-					default:
-					  methods.createSelectBox();
-				}			
-			
-			},
+                
+            init: function() {
+                
+                return this.each(function () {
+                    methods.appendHTML();
+                    methods.setEventHandlers();                  
+                });
+            },
+            
+            /******************************
+            Append HTML
+            *******************************/            
+            
+            appendHTML: function() {
+            
+                switch(settings.type)
+                {
+                    case 'select':
+                        methods.createSelectBox();
+                        break;
+                      
+                    case 'list':
+                        methods.createList();
+                        break;
+                    
+                    case 'panel':
+                        methods.createPanel();
+                        break;
+                    
+                    default:
+                      methods.createSelectBox();
+                }            
+            
+            },
 
-			/******************************
-			Create List
-			*******************************/			
-			
-			createList: function() {
-				
-				var outputMarkup = '<a href="#" id="'+ settings.menuID +'-notch" class="nbs-aiorm-menu-notch"></a>';
-				
-				outputMarkup +=	'<div class="nbs-aiorm-wrap"><ul id="'+ settings.menuID +'" class="nbs-aiorm-list-menu">';
-				
-				var menuItems = object.find("li");
-				menuItems.each(function() {
-				
-					outputMarkup += '<li>'+$(this).html()+'</li>';
-				});
-				
-				outputMarkup += '</ul></div>';
-				
-				if(settings.targetContainer) {
-					$(outputMarkup).appendTo($(settings.targetContainer));
-				}
-				else 
-					$(outputMarkup).insertAfter($(object));
-			},				
+            /******************************
+            Create List
+            *******************************/            
+            
+            createList: function() {
+                
+                var outputMarkup = '<a href="#" id="'+ settings.menuID +'-notch" class="nbs-aiorm-menu-notch"></a>';
+                
+                outputMarkup += '<div class="nbs-aiorm-wrap"><ul id="'+ settings.menuID +'" class="nbs-aiorm-list-menu">';
+                
+                var menuItems = object.find("li");
+                menuItems.each(function() {
+                    outputMarkup += '<li>'+$(this).html()+'</li>';
+                });
+                
+                outputMarkup += '</ul></div>';
+                
+                if(settings.targetContainer) {
+                    $(outputMarkup).appendTo($(settings.targetContainer));
+                }
+                else 
+                    $(outputMarkup).insertAfter($(object));
+            },                
 
-			/******************************
-			Create Select Box
-			*******************************/			
-			createSelectBox: function() {
-				var outputMarkup = '';				
-				outputMarkup +=	'<select id="'+ settings.menuID +'" class="nbs-aiorm-select-menu">';
-				outputMarkup += '<option value="#">....</option>'
-				var menuItems = object.find("li a");
-				menuItems.each(function(){
-					outputMarkup += '<option value="' + $(this).attr("href") + '">'+$(this).text()+'</option>';
-				});
-				outputMarkup += '</select>';
-				
-				if(settings.targetContainer) {
-					$(outputMarkup).appendTo($(settings.targetContainer));
-				}
-				else 
-					$(outputMarkup).insertAfter($(object));
-			},
-			
-			/******************************
-			Create Panel
-			*******************************/			
-			createPanel: function() {
-			
-				var notchMarkup = '<a href="#" id="'+ settings.menuID +'-notch" class="nbs-aiorm-menu-notch"></a>'
-				var outputMarkup = '<div class="nbs-aiorm-wrap"><div class="nbs-aiorm-panel"><a href="#" class="nbs-aiorm-panel-close"></a>';
-				
-				outputMarkup +=	'<ul id="'+ settings.menuID +'" class="nbs-aiorm-panel-menu">';
+            /******************************
+            Create Select Box
+            *******************************/            
+            createSelectBox: function() {
+                var outputMarkup = '';                
+                outputMarkup +=    '<select id="'+ settings.menuID +'" class="nbs-aiorm-select-menu">';
+                outputMarkup += '<option value="#">....</option>'
+                var menuItems = object.find("li a");
+                menuItems.each(function(){
+                    outputMarkup += '<option value="' + $(this).attr("href") + '">'+$(this).text()+'</option>';
+                });
+                outputMarkup += '</select>';
+                
+                if(settings.targetContainer) {
+                    $(outputMarkup).appendTo($(settings.targetContainer));
+                }
+                else 
+                    $(outputMarkup).insertAfter($(object));
+            },
+            
+            /******************************
+            Create Panel
+            *******************************/            
+            createPanel: function() {
+            
+                var notchMarkup = '<a href="#" id="'+ settings.menuID +'-notch" class="nbs-aiorm-menu-notch"></a>'
+                var outputMarkup = '<div class="nbs-aiorm-wrap"><div class="nbs-aiorm-panel"><a href="#" class="nbs-aiorm-panel-close"></a>';
+                
+                outputMarkup +=    '<ul id="'+ settings.menuID +'" class="nbs-aiorm-panel-menu">';
 
-				var menuItems = object.find("li");
-				menuItems.each(function() {
-				
-					outputMarkup += '<li>'+$(this).html()+'</li>';
-				});
-				outputMarkup += '</ul></div></div>';
-				
-				if(settings.targetContainer) {
-					$(notchMarkup).appendTo($(settings.targetContainer));
-				}
-				else 
-					$(notchMarkup).insertAfter($(object));
-					
-				$(outputMarkup).prependTo('body');			
-			},
-			
-			/******************************
-			Handle Click
-			*******************************/			
-			handleClick: function(){
-				
-				switch(settings.type)
-				{
-					  
-					case 'list':
-						$('#'+settings.menuID).slideToggle();
-						break;
-					
-					case 'panel':
-						$('.nbs-aiorm-panel').css('display', 'block');
-						break;
-					
-					default:
-					  return;
-				}				
-				
-			},
-			
-			/******************************
-			Set Event Handlers
-			*******************************/
-			setEventHandlers: function() {
+                var menuItems = object.find("li");
+                menuItems.each(function() {
+                
+                    outputMarkup += '<li>'+$(this).html()+'</li>';
+                });
+                outputMarkup += '</ul></div></div>';
+                
+                if(settings.targetContainer) {
+                    $(notchMarkup).appendTo($(settings.targetContainer));
+                }
+                else 
+                    $(notchMarkup).insertAfter($(object));
+                    
+                $(outputMarkup).prependTo('body');            
+            },
+            
+            /******************************
+            Handle Click
+            *******************************/            
+            handleClick: function(){
+                
+                switch(settings.type)
+                {
+                      
+                    case 'list':
+                        $('#'+settings.menuID).slideToggle();
+                        break;
+                    
+                    case 'panel':
+                        $('.nbs-aiorm-panel').css('display', 'block');
+                        break;
+                    
+                    default:
+                      return;
+                }                
+                
+            },
+            
+            /******************************
+            Set Event Handlers
+            *******************************/
+            setEventHandlers: function() {
 
-				$('body').on('change', '#' + settings.menuID, function() {  
-					if ($(this).val()!='') {
-						window.location.href=$(this).val();
-					}
-				});
-				
-				$('#'+settings.menuID+'-notch').click(function(){
-					methods.handleClick();
-					return false;
-				});
-				
-				$('.nbs-aiorm-panel-close').click(function(){
-					$('.nbs-aiorm-panel').css('display','none');
-					return false;
-				});				
-				
-			}
+                $('body').on('change', '#' + settings.menuID, function() {  
+                    if ($(this).val()!='') {
+                        window.location.href=$(this).val();
+                    }
+                });
+                
+                $('#'+settings.menuID+'-notch').click(function(){
+                    methods.handleClick();
+                    return false;
+                });
+                
+                $('.nbs-aiorm-panel-close').click(function(){
+                    $('.nbs-aiorm-panel').css('display','none');
+                    return false;
+                });                
+                
+            }
         
         };
         
-        if (methods[options]) { 	// $("#element").pluginName('methodName', 'arg1', 'arg2');
+        if (methods[options]) {     // $("#element").pluginName('methodName', 'arg1', 'arg2');
             return methods[options].apply(this, Array.prototype.slice.call(arguments, 1));
-        } else if (typeof options === 'object' || !options) { 	// $("#element").pluginName({ option: 1, option:2 });
+        } else if (typeof options === 'object' || !options) {     // $("#element").pluginName({ option: 1, option:2 });
             return methods.init.apply(this);  
         } else {
             $.error( 'Method "' +  method + '" does not exist in responsiveMenu plugin!');
         }        
-};
+    };
 
 })(jQuery);
